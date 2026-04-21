9017b442838da043829bb9948314b0fa64fdec5c
girlJIJI99 <27zoeandrea00@gmail.com>
2026-04-20 22:36:14 -0700

    Segundo commit integracion de las 4 clases

--- pokemon_clases.py
+++ pokemon_clases.py
@@ -1,5 +1,8 @@
 #Clae base abstracta
-class Pokemon:
+from abc import ABC, abstractmethod
+import random
+
+class Pokemon(ABC):
     def __init__(self, nombre, hp_maximo, energia_maxima):
         self.nombre = nombre
         self.__hp_actual  = hp_maximo 
@@ -13,6 +16,7 @@ class Pokemon:
     def hp_actual(self):
         return self.__hp_actual
     #Settter no permite negativos 
+
     @hp_actual.setter
     def hp_actual(self, valor):
         if valor < 0:
@@ -38,8 +42,97 @@ class Pokemon:
             self.defendiendo = True
             print(f"{self.nombre} se está defendiendo")
         else:
-            print("No tienes energía suficiente")
+            print("No tienes  energía suficiente" )
 
     def descansar(self):
         self.energia_actual += 20
-        print(f"{self.nombre} recupera energía")
+        print(f"{self.nombre}   recupera energía" )
+    
+    @abstractmethod
+    def atacar(self, oponente):
+        pass
+
+# 4 CLASES HIJAS esenciales que heredan de la clase base "POKEMON"
+#CLASE AGUA CON ISINSTANCE
+class PokemonAgua(Pokemon):
+    def atacar(self, oponente):
+        #Validacion principal 
+        if self.energia_actual <15:
+            print("No cuenta con energia suficente.")
+            return
+        self.energia_actual -= 15
+        daño = 10
+#--------------- Segunda y tercera Validacion
+        if isinstance(oponente, PokemonFuego):
+            daño *= 2
+            print("Es super efectivo.")
+        if oponente.defendiendo:
+            daño //= 2
+            oponente.defendiendo = False
+        oponente.hp_actual -= daño
+        print(f"{self.nombre} hace {daño} de daño")
+
+
+#CALSE FUEGO CON INSINSTANCE
+class PokemonFuego(Pokemon):
+    def atacar (self, oponente):
+        if self.energia_actual <15:
+            print("No tiene energia sufciente")
+            return
+        self.energia_actual -=15
+        daño = 10
+#---------------Segunda y tercera Validacion
+        if isinstance(oponente, PokemonPlanta):
+            daño *= 2
+            print("Es super efectivo.")
+        if oponente.defendiendo:
+            daño //= 2
+            oponente.defendiendo = False
+        oponente.hp_actual -= daño
+        print(f"{self.nombre} hace {daño} de daño")
+
+#CLASE PLANTA CON INSINSTANCE
+
+class PokemonPlanta(Pokemon):
+    def atacar(self, oponente):
+        if self.energia_actual <15:
+            print("No tiene energia suficiente")
+            return
+        self.energia_actual -= 15
+        daño = 10 
+#---------------Segunda y tercera Validacion
+        if isinstance(oponente, PokemonAgua):
+            daño *= 2
+            print("Es super efectivo.")
+        if oponente.defendiendo:
+            daño //= 2
+            oponente.defendiendo = False
+
+        oponente.hp_actual -= daño
+        print(f"{self.nombre} hace {daño} de daño")
+
+#CLASE POKEMON ELECTRICO(POKEMON):
+class PokemonElectrico(Pokemon):
+    def atacar(self, oponente):
+        if self.energia_actual<15:
+            print("No tiene energia suficiente")
+            return
+        self.energia_actual -= 15
+        daño =10
+
+#--------------Segunda y tercera validacion
+
+        if  random.random() <0.2:
+            print("El oponente quedo paralizado.")
+            oponente.paralizado = True 
+        if oponente.defendiendo:
+            daño//=2
+            oponente.defendiendo = False
+
+            oponente.hp_actual -= daño
+            print(f"{self.nombre} hace {daño} de daño")
+
+
+
+
+
